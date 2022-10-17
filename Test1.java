e849d9ae73e2aa7ae5c8312450e61f214200e67f
smokerkingchanchal <110466164+smokerkingchanchal@users.noreply.github.com>
2022-10-17 02:34:32 -0700

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,13 @@
+// Addition of two number
+public class Test1{
+    public static void main(String[] args){
+    // initialize 1st value of local variable
+     int a=5;
+    //  initialize 2nd value of local variable
+    int b = 6;
+    System.out.println("two numbers is :"+a+" "+b);
+    // logic part
+    int sum = a+b;
+    System.out.println("The sum of two numbers is :"+sum);
+}
+}
