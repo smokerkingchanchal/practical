eae20c02746c124663ea47797c411235d41fbeba
smokerkingchanchal <110466164+smokerkingchanchal@users.noreply.github.com>
2022-10-17 04:11:51 -0700

    Add files via upload

---
+++ TryCtach.java
@@ -0,0 +1,12 @@
+public class TryCtach {
+   public static void main (String[] args) {
+  try {
+      int num = 25/0;  
+     System.out.println("Tao code run hobe");
+} catch (ArithmeticException r) { // handing exception
+    System.out.println(r)
+   {
+     System.out.println(25/5);
+}
+
+}}
