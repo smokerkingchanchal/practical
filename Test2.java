5945e595fbb98d220520a6c2d75e402301ba3196
smokerkingchanchal <110466164+smokerkingchanchal@users.noreply.github.com>
2022-10-17 02:38:02 -0700

    Add files via upload

---
+++ Test2.java
@@ -0,0 +1,13 @@
+// substraction between two numbers
+public class Test2{
+    public static void main(String[] args){
+    // initialize 1st value of local variable
+    int A=10;
+    // initialize 2nd value of local variable
+    int B=5;
+    System.out.println("Two numbers is :"+A+" "+B);
+    // logic part
+    int sub =A-B;
+    System.out.println("The substraction between two numbers is :"+sub);
+    }
+}
