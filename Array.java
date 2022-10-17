5b1b20d6f41ac70140941071b0f69ee232fcfc59
smokerkingchanchal <110466164+smokerkingchanchal@users.noreply.github.com>
2022-10-17 02:27:36 -0700

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,11 @@
+class A{
+    static int c =30;
+    int a = 20;
+    public static void main(String[] args){
+      A obj = new A();
+      int b = 10;
+      System.out.println(A.c);
+      System.out.println(b);
+      System.out.println(obj.a);  
+    }
+}
